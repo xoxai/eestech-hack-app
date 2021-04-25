8421225728a6c0df9ab960ed02d1f55e5c5a5339
Evgenii Prosvirnin <proevgenii19@gmail.com>
2021-04-26 02:00:03 +0300

    ПОЧИНИТЕ МЕТОД store() в классе TableView в sedan/views.py. В с должно быть Client(fio =фио из первой строки файла,sex = пол из первой строки файла,  age = аналогично,phone =аналогично, email= аналогично ) Т.е надо построчно читать файл просто

--- sedan/serializers.py
+++ sedan/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from sedan.models import  Data_stor
+from sedan.models import  Data_stor, Client
 
 class Data_uploader(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault()) #Скрываем поле юзер, автоматически вставляет из реквеста
     class Meta:
-        model = Data_stor
+        model = Data_stor, Client
         fields = '__all__'
 
 class Data_view_serializer(serializers.ModelSerializer):
--- sedan/views.py
+++ sedan/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
+import pandas as pd
+
 from rest_framework import generics
 from sedan.serializers import Data_uploader, Data_view_serializer
-from sedan.models import Data_stor
-from rest_framework.permissions import IsAuthenticated,
+from sedan.models import Data_stor, Client
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 # Create your views here.
 class DataCreateView(generics.CreateAPIView):
     serializer_class = Data_uploader
     permission_classes = (IsAuthenticated,)
 
+
 class TableView(generics.ListAPIView):
     serializer_class = Data_view_serializer
-    queryset = Data_stor.objects.all()
+    queryset = Client.objects.all()
     permission_classes = (IsAuthenticated,)
 
 class Table_View_Editing(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = Data_uploader
     queryset = Data_stor.objects.all()
     permission_classes = (IsAuthenticated,)
+
+
+class TableView(generics.CreateAPIView):
+    serializer_class = Data_view_serializer
+    queryset = Data_stor.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def store(self):
+        data = pd.read_excel(r'E:\SEDAN\eestech-hack-app\data.xlsx',sheet_name=None)
+        for c in data.iterrows()[1]:
+            c = Client(fio = c[0],sex = c[1],
+                        age = c[2],
+                        phone =c[3], email= c[4] )
+            c.save()
+        return c.save()
